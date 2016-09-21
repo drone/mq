c5142b33c5bff61986ec0ef2c5d433ff0b4e0cb9
Brad Rydzewski <brad.rydzewski@gmail.com>
2016-09-21 11:13:04 -0500

    added unit tests for messages

--- stomp/message_test.go
+++ stomp/message_test.go
@@ -1,1 +1,149 @@
 package stomp
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestMessageCopy(t *testing.T) {
+	m := NewMessage()
+	m.ID = 1
+	m.Proto = []byte("1.2")
+	m.Method = MethodSend
+	m.User = []byte("username")
+	m.Pass = []byte("password")
+	m.Dest = []byte("/topic/test")
+	m.Subs = 1
+	m.Ack = AckAuto
+	m.Prefetch = []byte("2")
+	m.Selector = []byte("ram >= 2")
+	m.Persist = PersistTrue
+	m.Retain = RetainAll
+	m.Receipt = []byte("1")
+	m.Body = []byte("hello world")
+	m.Header.Add([]byte("key"), []byte("val"))
+
+	c := m.Copy()
+
+	if m.ID != c.ID {
+		t.Errorf("expect ID value is copied")
+	}
+	if !bytes.Equal(m.Proto, c.Proto) {
+		t.Errorf("expect Proto value is copied")
+	}
+	if !bytes.Equal(m.Method, c.Method) {
+		t.Errorf("expect Method value is copied")
+	}
+	if !bytes.Equal(m.User, c.User) {
+		t.Errorf("expect User value is copied")
+	}
+	if !bytes.Equal(m.Pass, c.Pass) {
+		t.Errorf("expect Pass value is copied")
+	}
+	if !bytes.Equal(m.Dest, c.Dest) {
+		t.Errorf("expect Dest value is copied")
+	}
+	if m.Subs != c.Subs {
+		t.Errorf("expect Subs value is copied")
+	}
+	if !bytes.Equal(m.Ack, c.Ack) {
+		t.Errorf("expect Ack value is copied")
+	}
+	if !bytes.Equal(m.Prefetch, c.Prefetch) {
+		t.Errorf("expect Prefetch value is copied")
+	}
+	if !bytes.Equal(m.Selector, c.Selector) {
+		t.Errorf("expect Selector value is copied")
+	}
+	if !bytes.Equal(m.Persist, c.Persist) {
+		t.Errorf("expect Persist value is copied")
+	}
+	if !bytes.Equal(m.Retain, c.Retain) {
+		t.Errorf("expect Retain value is copied")
+	}
+	if !bytes.Equal(m.Receipt, c.Receipt) {
+		t.Errorf("expect Receipt value is copied")
+	}
+	if !bytes.Equal(m.Body, c.Body) {
+		t.Errorf("expect Body value is copied")
+	}
+	if m.Header.itemc != c.Header.itemc {
+		t.Errorf("expect Header items are copied")
+	}
+	// for good measure, let's make sure that altering the copy
+	// will not alter the original.
+	c.Body = []byte("bonjour monde!")
+	if bytes.Equal(m.Body, c.Body) {
+		t.Errorf("expect updating the Body does not alter original")
+	}
+	c.Header.items[0].data = []byte("foo")
+	if bytes.Equal(m.Header.items[0].data, c.Header.items[0].data) {
+		t.Errorf("expect updating the Header does not alter original")
+	}
+}
+
+func TestMessageRelease(t *testing.T) {
+	m := NewMessage()
+	m.ID = 1
+	m.Proto = []byte("1.2")
+	m.Method = MethodSend
+	m.User = []byte("username")
+	m.Pass = []byte("password")
+	m.Dest = []byte("/topic/test")
+	m.Subs = 1
+	m.Ack = AckAuto
+	m.Prefetch = []byte("2")
+	m.Selector = []byte("ram >= 2")
+	m.Persist = PersistTrue
+	m.Retain = RetainAll
+	m.Receipt = []byte("1")
+	m.Body = []byte("hello world")
+	m.Header.Add([]byte("key"), []byte("val"))
+	m.Release()
+
+	if m.ID != 0 {
+		t.Errorf("expect ID to reset to zero value")
+	}
+	if len(m.Proto) != 0 {
+		t.Errorf("expect Proto to reset to zero value")
+	}
+	if len(m.Method) != 0 {
+		t.Errorf("expect Method to reset to zero value")
+	}
+	if len(m.User) != 0 {
+		t.Errorf("expect User to reset to zero value")
+	}
+	if len(m.Pass) != 0 {
+		t.Errorf("expect Pass to reset to zero value")
+	}
+	if len(m.Dest) != 0 {
+		t.Errorf("expect Dest to reset to zero value")
+	}
+	if m.Subs != 0 {
+		t.Errorf("expect Subs to reset to zero value")
+	}
+	if len(m.Ack) != 0 {
+		t.Errorf("expect Ack to reset to zero value")
+	}
+	if len(m.Prefetch) != 0 {
+		t.Errorf("expect Prefetch to reset to zero value")
+	}
+	if len(m.Selector) != 0 {
+		t.Errorf("expect Selector to reset to zero value")
+	}
+	if len(m.Persist) != 0 {
+		t.Errorf("expect Persist to reset to zero value")
+	}
+	if len(m.Retain) != 0 {
+		t.Errorf("expect Retain to reset to zero value")
+	}
+	if len(m.Receipt) != 0 {
+		t.Errorf("expect Receipt to reset to zero value")
+	}
+	if len(m.Body) != 0 {
+		t.Errorf("expect Body to reset to zero value")
+	}
+	if m.Header.itemc != 0 {
+		t.Errorf("expect Header to reset to zero value")
+	}
+}
--- stomp/options_test.go
+++ stomp/options_test.go
@@ -1,1 +1,74 @@
 package stomp
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestOptions(t *testing.T) {
+	opt := WithAck("auto")
+	msg := NewMessage()
+	msg.Apply(opt)
+	if !bytes.Equal(msg.Ack, AckAuto) {
+		t.Errorf("Want WiathAck to apply ack header")
+	}
+
+	opt = WithCredentials("janedoe", "password")
+	msg = NewMessage()
+	msg.Apply(opt)
+	if string(msg.User) != "janedoe" {
+		t.Errorf("Want WithCredentials to apply username header")
+	}
+	if string(msg.Pass) != "password" {
+		t.Errorf("Want WithCredentials to apply password header")
+	}
+
+	opt = WithExpires(1234)
+	msg = NewMessage()
+	msg.Apply(opt)
+	if msg.Expires != 1234 {
+		t.Errorf("Want WithExpires to apply expires header")
+	}
+
+	opt = WithHeader("foo", "bar")
+	msg = NewMessage()
+	msg.Apply(opt)
+	if v := msg.Header.Get([]byte("foo")); string(v) != "bar" {
+		t.Errorf("Want WithHeader to add header keypair")
+	}
+
+	opt = WithPersistence()
+	msg = NewMessage()
+	msg.Apply(opt)
+	if !bytes.Equal(msg.Persist, PersistTrue) {
+		t.Errorf("Want WithPersistence to apply persist header")
+	}
+
+	opt = WithPrefetch(2)
+	msg = NewMessage()
+	msg.Apply(opt)
+	if !bytes.Equal(msg.Prefetch, []byte("2")) {
+		t.Errorf("Want WithPrefetch to apply persist header")
+	}
+
+	opt = WithReceipt()
+	msg = NewMessage()
+	msg.Apply(opt)
+	if len(msg.Receipt) == 0 {
+		t.Errorf("Want WithReceipt to apply receipt header")
+	}
+
+	opt = WithRetain("last")
+	msg = NewMessage()
+	msg.Apply(opt)
+	if !bytes.Equal(msg.Retain, RetainLast) {
+		t.Errorf("Want WithRetain to apply retain header")
+	}
+
+	opt = WithSelector("ram > 2")
+	msg = NewMessage()
+	msg.Apply(opt)
+	if !bytes.Equal(msg.Selector, []byte("ram > 2")) {
+		t.Errorf("Want WithRetain to apply retain header")
+	}
+}
