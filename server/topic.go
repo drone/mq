6af9c4e90afac333417d3910c4701ecf1059b674
Brad Rydzewski <brad.rydzewski@gmail.com>
2016-09-22 14:54:57 -0500

    add topic queue with unit tests

---
+++ server/topic.go
@@ -0,0 +1,123 @@
+package server
+
+import (
+	"bytes"
+	"math/rand"
+	"sync"
+
+	"github.com/drone/mq/stomp"
+)
+
+// topic is a type of destination handler that implements a
+// publish subscribe pattern. Subscribers to a topic receive
+// all messages from the publisher.
+type topic struct {
+	sync.RWMutex
+
+	dest []byte
+	hist []*stomp.Message
+	subs map[*subscription]struct{}
+}
+
+func newTopic(dest []byte) *topic {
+	return &topic{
+		dest: dest,
+		subs: make(map[*subscription]struct{}),
+	}
+}
+
+// publishes a copy of the message to the subsciber list.
+// If the message includes the retain:true headers the message is
+// saved for future use. If the message includes retain:remove the
+// previously retained message is set to nil.
+func (t *topic) publish(m *stomp.Message) error {
+	id := rand.Int63()
+
+	t.RLock()
+	for sub := range t.subs {
+		if sub.selector != nil {
+			if ok, _ := sub.selector.Eval(m.Header); !ok {
+				continue
+			}
+		}
+		c := m.Copy()
+		c.ID = id
+		c.Method = stomp.MethodMessage
+		sub.session.send(c)
+	}
+	t.RUnlock()
+
+	// if a message has the retain header set we should either
+	// retain the message, or remove the existing retained message.
+	if len(m.Retain) != 0 {
+		t.Lock()
+		switch {
+		case bytes.Equal(m.Retain, stomp.RetainLast):
+			if len(t.hist) == 1 {
+				t.hist[0] = m
+			} else {
+				t.hist = t.hist[:0]
+				t.hist = append(t.hist, m)
+			}
+		case bytes.Equal(m.Retain, stomp.RetainAll):
+			t.hist = append(t.hist, m)
+		case bytes.Equal(m.Retain, stomp.RetainRemove):
+			t.hist = t.hist[:0]
+		}
+		t.Unlock()
+	}
+
+	return nil
+}
+
+// registers the subscription with the topic broker and
+// sends the last retained message, if one exists.
+func (t *topic) subscribe(s *subscription, m *stomp.Message) error {
+	t.Lock()
+	t.subs[s] = struct{}{}
+	t.Unlock()
+
+	t.RLock()
+	hist := make([]*stomp.Message, len(t.hist))
+	copy(hist, t.hist)
+	t.RUnlock()
+
+	for _, m := range hist {
+		c := m.Copy()
+		c.Method = stomp.MethodMessage
+		c.ID = rand.Int63()
+		s.session.send(c)
+	}
+
+	return nil
+}
+
+func (t *topic) unsubscribe(s *subscription, m *stomp.Message) error {
+	t.Lock()
+	delete(t.subs, s)
+	t.Unlock()
+	return nil
+}
+
+func (t *topic) disconnect(s *session) error {
+	t.Lock()
+	for _, subscription := range s.sub {
+		delete(t.subs, subscription)
+	}
+	t.Unlock()
+	return nil
+}
+
+// returns true if the topic has zero subscribers indicating
+// that it can be recycled.
+func (t *topic) recycle() (ok bool) {
+	t.RLock()
+	ok = len(t.subs) == 0 && len(t.hist) == 0
+	t.RUnlock()
+	return
+}
+
+// return the destination name.
+func (t *topic) destination() string {
+	return string(t.dest)
+}
---
+++ server/topic_test.go
@@ -0,0 +1,176 @@
+package server
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/drone/mq/stomp"
+)
+
+func Test_topic_publish(t *testing.T) {
+	m := stomp.NewMessage()
+	m.Dest = []byte("/topic/test")
+	m.Body = []byte("hello")
+	m.Selector = []byte("skip != true")
+	defer m.Release()
+
+	peer, client := stomp.Pipe()
+	sess := requestSession()
+	sess.peer = peer
+	defer releaseSession(sess)
+
+	s := sess.subs(m)
+	b := newTopic(m.Dest)
+	b.subscribe(s, m)
+	b.publish(m)
+
+	select {
+	case got := <-client.Receive():
+		if !bytes.Equal(got.Body, m.Body) {
+			t.Errorf("expect message published to topic subscription")
+		}
+	default:
+		t.Errorf("expect message published and delivered")
+	}
+
+	skip := stomp.NewMessage()
+	skip.Header.Add([]byte("skip"), []byte("true"))
+	skip.Body = []byte("skip me")
+	b.publish(skip)
+	select {
+	case <-client.Receive():
+		t.Errorf("expect the selector to filter out the message.")
+	default:
+		// expected
+	}
+}
+
+func Test_topic_publish_retain(t *testing.T) {
+	m := stomp.NewMessage()
+	m.Dest = []byte("/topic/test")
+	m.Body = []byte("hello")
+	m.Retain = stomp.RetainLast
+	defer m.Release()
+
+	b := newTopic(m.Dest)
+	b.publish(m)
+	if len(b.hist) != 1 || !bytes.Equal(b.hist[0].Body, m.Body) {
+		t.Errorf("expected topic retained message")
+	}
+
+	m.Retain = stomp.RetainLast
+	m.Body = []byte("hello2")
+	b.publish(m)
+	if len(b.hist) != 1 || !bytes.Equal(b.hist[0].Body, m.Body) {
+		t.Errorf("expected topic retained message to update")
+	}
+
+	m.Retain = stomp.RetainAll
+	m.Body = []byte("hello")
+	b.publish(m)
+	if len(b.hist) != 2 {
+		t.Errorf("expected topic retained message to append")
+	}
+
+	m.Retain = stomp.RetainRemove
+	b.publish(m)
+	if len(b.hist) != 0 {
+		t.Errorf("expected topic retained message removed")
+	}
+}
+
+func Test_topic_subscribe(t *testing.T) {
+	peer, client := stomp.Pipe()
+	sess := requestSession()
+	sess.peer = peer
+	defer releaseSession(sess)
+
+	msg1 := stomp.NewMessage()
+	msg1.Method = stomp.MethodSend
+	msg1.Dest = []byte("/topic/test")
+	msg1.Body = []byte("hello")
+	msg1.Retain = stomp.RetainAll
+	defer msg1.Release()
+
+	msg2 := stomp.NewMessage()
+	msg2.Method = stomp.MethodSubscribe
+	msg2.Dest = []byte("/topic/test")
+	defer msg2.Release()
+
+	msg3 := stomp.NewMessage()
+	msg3.Method = stomp.MethodUnsubscribe
+	msg3.Dest = []byte("/topic/test")
+	defer msg3.Release()
+
+	brok := newTopic(msg1.Dest)
+	brok.publish(msg1)
+
+	sub := sess.subs(msg2)
+	defer sess.unsub(sub)
+
+	brok.subscribe(sub, msg2)
+	if _, ok := brok.subs[sub]; !ok {
+		t.Errorf("want subscription added to topic")
+	}
+
+	got := <-client.Receive()
+	if !bytes.Equal(got.Body, msg1.Body) {
+		t.Errorf("want retained message sent to subscriber")
+	}
+
+	brok.unsubscribe(sub, msg3)
+	if _, ok := brok.subs[sub]; ok {
+		t.Errorf("want subscription removed from topic")
+	}
+}
+
+func Test_topic_disconnect(t *testing.T) {
+	sess := requestSession()
+	defer releaseSession(sess)
+
+	msg := stomp.NewMessage()
+	msg.Dest = []byte("/topic/test")
+	defer msg.Release()
+
+	sub := sess.subs(msg)
+	defer sess.unsub(sub)
+
+	brok := newTopic(msg.Dest)
+	brok.subscribe(sub, msg)
+	if _, ok := brok.subs[sub]; !ok {
+		t.Errorf("want subscription added to topic")
+	}
+
+	brok.disconnect(sess)
+	if _, ok := brok.subs[sub]; ok {
+		t.Errorf("want subscription removed from topic on disconnect")
+	}
+}
+
+func Test_topic_recycle(t *testing.T) {
+	dest := []byte("/topic/test")
+	brok := newTopic(dest)
+	if !brok.recycle() {
+		t.Errorf("want recycle true when no subscribers")
+	}
+	msg := stomp.NewMessage()
+	defer msg.Release()
+
+	brok.hist = []*stomp.Message{msg}
+	if brok.recycle() {
+		t.Errorf("want recycle false when no subscribers but retained message")
+	}
+	brok.hist = brok.hist[:0]
+	brok.subs[&subscription{}] = struct{}{}
+	if brok.recycle() {
+		t.Errorf("want recycle false when subscribers")
+	}
+}
+
+func Test_topic_dest(t *testing.T) {
+	dest := []byte("/topic/test")
+	brok := newTopic(dest)
+	if got := brok.destination(); got != "/topic/test" {
+		t.Errorf("want destingation name /topic/test got %s", got)
+	}
+}
