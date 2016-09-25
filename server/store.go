7d261955a7d2d647800e248c449782e28bed6de6
Brad Rydzewski <brad.rydzewski@gmail.com>
2016-09-25 18:21:08 -0500

    added store, which is not yet implemented

---
+++ server/store.go
@@ -0,0 +1,50 @@
+package server
+
+import (
+	"github.com/drone/mq/stomp"
+
+	"github.com/syndtr/goleveldb/leveldb"
+)
+
+type store interface {
+	put(*stomp.Message) error
+	delete(*stomp.Message) error
+	close() error
+}
+
+type datastore struct {
+	db *leveldb.DB
+}
+
+func (d *datastore) put(m *stomp.Message) error {
+	return d.db.Put(m.ID, m.Bytes(), nil)
+}
+
+func (d *datastore) delete(m *stomp.Message) error {
+	return d.db.Delete(m.ID, nil)
+}
+
+func (d *datastore) close() error {
+	return d.db.Close()
+}
+
+// loadDatastore reads the datastore from disk and restores
+// persisted message to the appropriate queues.
+func loadDatastore(path string, b *router) (store, error) {
+	db, err := leveldb.RecoverFile(path, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	// iterate through the persisted messages
+	// and send to the broker.
+	iter := db.NewIterator(nil, nil)
+	for iter.Next() {
+		m := stomp.NewMessage()
+		m.Parse(iter.Value())
+		b.publish(m)
+	}
+	iter.Release()
+
+	return &datastore{db: db}, nil
+}
